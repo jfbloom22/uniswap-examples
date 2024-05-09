378357811354e09097af17804604f202182185ac
Jonathan Flower <jfbloom22@gmail.com>
2024-05-09 09:57:28 -0400

    added improvements found on other PRs

--- v3-sdk/quoting/src/libs/constants.ts
+++ v3-sdk/quoting/src/libs/constants.ts
@@ -1,6 +1,6 @@
 // This file stores web3 related constants such as addresses, token definitions, ETH currency references and ABI's
 
-import { Token } from '@uniswap/sdk-core'
+import { Token, ChainId } from '@uniswap/sdk-core'
 
 // Addresses
 
@@ -12,7 +12,7 @@ export const QUOTER_CONTRACT_ADDRESS =
 // Currencies and Tokens
 
 export const WETH_TOKEN = new Token(
-  8453,
+  ChainId.BASE,
   '0x4200000000000000000000000000000000000006',
   18,
   'WETH',
@@ -20,7 +20,7 @@ export const WETH_TOKEN = new Token(
 )
 
 export const USDC_TOKEN = new Token(
-  8453,
+  ChainId.BASE,
   '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
   6,
   'USDC',
